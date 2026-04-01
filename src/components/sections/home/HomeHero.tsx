6c0af26e754cb738afda0f57d459bf94c1afcaa5
helios231194 <helios231194@gmail.com>
2026-04-01 16:29:08 +0700

    style: remove numeric slide counter from HomeHero to clean up UI

--- src/components/sections/home/HomeHero.tsx
+++ src/components/sections/home/HomeHero.tsx
@@ -147,12 +147,8 @@ export function HomeHero() {
       {/* Bottom Controls: Slide Counter & Dots */}
       <div className="absolute bottom-6 md:bottom-10 left-0 right-0 z-30 container mx-auto px-4 md:px-6 flex items-end justify-between">
         
-        {/* Counter */}
-        <div className="text-white/60 text-sm font-mono flex flex-col gap-1 w-24">
-          <span className="text-white font-black text-xl leading-none">{String(current + 1).padStart(2, '0')}</span>
-          <span className="w-6 h-px bg-white/30" />
-          <span className="text-xs">{String(slides.length).padStart(2, '0')}</span>
-        </div>
+        {/* Counter - Removed as requested */}
+        <div className="w-24" />
 
         {/* Scroll down indicator */}
         <div className="hidden md:flex flex-col items-center gap-2 animate-bounce opacity-50 flex-1">
